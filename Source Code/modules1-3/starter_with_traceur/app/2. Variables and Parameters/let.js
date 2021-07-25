5f6f29bc01b43ea7995979647f722d1e5bb13515
StevenJoseph19 <stevesam1927@gmail.com>
2021-07-25 13:29:58 +0530

    Slides and upto date Source Code from Hands On Training Sessions for modules 1-3

--- Source Code/modules1-3/starter_with_traceur/app/2. Variables and Parameters/let.js
+++ Source Code/modules1-3/starter_with_traceur/app/2. Variables and Parameters/let.js
@@ -34,6 +34,31 @@ describe("how let works", function(){
 		var result = doWork();
 		expect(result).toBe(10);
 
-	});	
+	});
+	
+	 it("will make a variable read-only", function() {  	
+  	
+    const MAX_SIZE = 10;
+	    
+    // MAX_SIZE = 12; //SyntaxError
+    
+    expect(MAX_SIZE).toBe(10);
+
+	 });
+	
+// 	 it("can shadow outer declaration", function() {  	
+  	
+//   		// const x = 12;
+//     	var doWork = function(){
+    		
+//         let x = 12;
+//         var x = 10;
+//     		return x;
+//     	};
+
+//     	var result = doWork();
+//     	expect(result).toBe(10);
+//     	// expect(x).toBe(12);
+//    });
 });
 
